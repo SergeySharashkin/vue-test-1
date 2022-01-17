664a9858ea478fedcbdc546677a410fcf33e38a9
SergeySharashkin <S_sharashkin@mail.ru>
2022-01-17 03:35:36 +0300

    ddd

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
-export const css = {
-    loaderOptions: {
-        sass: {
-            prependData: '@import "@/assets/styles.scss"'
-        }
-    }
-};
+// export const css = {
+//     loaderOptions: {
+//         sass: {
+//             prependData: '@import "@/assets/styles.scss"'
+//         }
+//     }
+// };
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vue-test-1/'
+      : '/'
+  }
